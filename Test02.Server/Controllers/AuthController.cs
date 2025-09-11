2db88c22394698914d04927f305addd464e6753f
strn02 <nekoduki0802@gmail.com>
2025-09-11 13:50:56 +0900

    ユーザー一覧

--- Test02.Server/Controllers/AuthController.cs
+++ Test02.Server/Controllers/AuthController.cs
@@ -9,10 +9,10 @@ namespace Test02.Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class AuthController : ControllerBase
+    public class UsersController : ControllerBase
     {
         private readonly NpgsqlConnection _connection;
-        public AuthController(NpgsqlConnection connection) => _connection = connection;
+        public UsersController(NpgsqlConnection connection) => _connection = connection;
         // ---- DTO ----
         public class LoginRequest
         {
--- test02.client/src/pages/UsersPage.jsx
+++ test02.client/src/pages/UsersPage.jsx
@@ -1,4 +1,1 @@
-﻿// src/pages/UsersPage.jsx
-export default function UsersPage() {
-    return <h1>ユーザー一覧</h1>;
-}
+﻿
